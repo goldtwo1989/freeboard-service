6fcee5ceec2014d2a9f7a74bc92fa5fd5c2f820b
goldtwo1989 <ttpd19891213@gmail.com>
2025-07-02 11:03:17 +0900

    init

---
+++ src/main/java/com/oopsw/seongsubean_freeboard_backend/vo/RequestFreeboard.java
@@ -0,0 +1,8 @@
+package com.oopsw.seongsubean_freeboard_backend.vo;
+
+import lombok.Data;
+
+@Data
+public class RequestFreeboard {
+
+}
---
+++ src/test/java/com/oopsw/seongsubean_freeboard_backend/SeongsubeanFreeboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oopsw.seongsubean_freeboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeongsubeanFreeboardBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
